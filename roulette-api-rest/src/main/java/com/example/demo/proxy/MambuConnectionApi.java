9bbc9a009debe56ca833793f47c2c9d498595668
ediguerrero <edixon_guerrero96@hotmail.com>
2022-01-18 16:11:10 -0500

    add_retrofit_basic_implementation_intructions

--- roulette-api-rest/src/main/java/com/example/demo/proxy/MambuConnectionApi.java
+++ roulette-api-rest/src/main/java/com/example/demo/proxy/MambuConnectionApi.java
@@ -6,7 +6,8 @@ import retrofit2.http.Header;
 import retrofit2.http.Query;
 
 public interface MambuConnectionApi {
-
+//@Body to send objets in body no ()
+    //@Query send objets in param
     @GET("account/transactions-filter")
     Call<Object> getMambuTransactionsByFilter(@Query("startDate") String startDate, @Query("endDate") String endDate, @Query("type") String type, @Query("offset") Integer offset, @Query("numberElements") Integer numberElements, @Header("instance")String instance);
 
